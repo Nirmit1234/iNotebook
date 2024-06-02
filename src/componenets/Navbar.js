423cd3c1f140bd8e6ed0e409885e4000ccc5e1ba
Nirmit1234 <119073449+Nirmit1234@users.noreply.github.com>
2024-06-02 12:47:42 +0530

    Update Navbar.js

--- src/componenets/Navbar.js
+++ src/componenets/Navbar.js
@@ -20,11 +20,7 @@ const Navbar = () => {
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <Link className={`nav-link ${location.pathname === "/"? "active" : ""}`} aria-current="page" to="/">Home</Link>
-        </li>
-      </ul>
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
       {!localStorage.getItem('token')?<form className='d-flex'>
           <Link className='btn btn-primary mx-1' to ='/login' role='button'>Login</Link>
           <Link className='btn btn-primary mx-1' to ='/signup' role='button'>Sign Up</Link>
